2138f63e2e3c39feea3c3773e3933107d6828de1
StevenWijaya-2008 <swijaya08@student.ciputra.ac.id>
2026-06-04 16:26:50 +0800

    Perbaikan avatar di homepage utk mengikuti username

--- src/main/resources/static/js/loginpage.js
+++ src/main/resources/static/js/loginpage.js
@@ -1,17 +1,6 @@
-function handleAnonymous() {
-    /* Karena saat ini kita fokus ke UI, kita langsung arahkan ke homepage */
-    window.location.href = "homepage.html";
-}
-
-
 function handleGoogle() {
     alert('Fitur login dengan Google belum tersedia. Silakan coba metode lain.');
 }
 function handleApple() {
     alert('Fitur login dengan Apple belum tersedia. Silakan coba metode lain.');
-}
-
-/* Fungsi untuk tombol "Sign in" (jika sudah punya akun) */
-function handleSignIn() {
-    window.location.href = "homepage.html";
 }
