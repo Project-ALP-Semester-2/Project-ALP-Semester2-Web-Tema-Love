f81d328964c1fea761c67f15417736f5c9f9a80e
StevenWijaya-2008 <swijaya08@student.ciputra.ac.id>
2026-06-04 13:21:31 +0800

    pembuatan proses backend signup

--- src/main/java/com/example/demo/Controller/LoginController.java
+++ src/main/java/com/example/demo/Controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Model.User;
+import com.example.demo.Repository.UserRepository;
 import com.example.demo.Service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,9 +12,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
     private final UserService userService;
+    private final UserRepository userRepository;
+    private final User user;
     
-    public LoginController(UserService userService) {
+    public LoginController(UserService userService, UserRepository userRepository, User user) {
         this.userService = userService;
+        this.userRepository = userRepository;
+        this.user = user;
     }
 
     @GetMapping("/login")
@@ -42,6 +48,39 @@ public class LoginController {
         }
     }
     
+    @GetMapping("/signup")
+    public String halamanSignUp() {
+        return "signup";
+    }
+
+    @PostMapping("/proses-signup")
+    public String prosesSignUp(
+            @RequestParam("username") String username,
+            @RequestParam("password") String password,
+            Model model) {
+
+
+        if (username.trim().isEmpty() || password.trim().isEmpty()) {
+            model.addAttribute("pesanError", "Username dan Password tidak boleh kosong!");
+            return "signuppage"; // Kembali ke halaman pendaftaran dengan pesan error
+        }
+
+        java.util.Optional<User> userExist = userRepository.findByUsername(username);
+        if (userExist.isPresent()) {
+            model.addAttribute("pesanError", "Username '" + username + "' sudah digunakan. Pilih username lain!");
+            return "signuppage"; 
+        }
+
+        User userBaru = new User();
+        userBaru.setUsername(username);
+        userBaru.setPassword(password); 
+
+
+        userRepository.save(userBaru);
+
+        return "redirect:/signin?suksesDaftar=true";
+    }
+
 
     @GetMapping("/homepage")
     public String halamanUtamaApp(@RequestParam(value = "userId", required = false) String userId, Model model) {
